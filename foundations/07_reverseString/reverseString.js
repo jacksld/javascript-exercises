df52a23b0179ee313d017413d2d991c9a5049eef
jack <145956234+jacksld@users.noreply.github.com>
2026-02-28 18:33:49 +0700

    Finish exercise 7
    Convert the string to an array storing each letter
    as array items.
    
    Loop over the array and add each letter to the
    beginning of an empty array.
    
    Convert the array was added with letters to
    string.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Separate string to array items
+    let letters = string.split("");
+    let reversedLetters = [];
+    // Get every single letter from the string
+    for (let i = 0; i < letters.length; i++) {
+        // Add each letter to the new array from the beginning
+        reversedLetters.unshift(letters[i]);
+    }
+    // Join the reversed letter together to form a string again
+    return reversedLetters.join("");
 };
 
 // Do not edit below this line
