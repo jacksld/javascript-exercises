210e8c7d7f797aa2b9d81dba9a7219aa42ea5202
jack <145956234+jacksld@users.noreply.github.com>
2026-02-28 17:34:49 +0700

    Finish exercise 6
    Create a new variable storing an empty string.
    Repeat the code that concatenate the empty string
    with the provided string with provided number of
    times.
    
    If the provided number of times is a negative
    value, return the text ERROR.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatTime) {
+    if (repeatTime < 0) return "ERROR";
+    let repeatingString = "";
+    for (let i = 0; i < repeatTime; i++) {
+        repeatingString += string;
+    }
+    return repeatingString;
 };
 
 // Do not edit below this line
