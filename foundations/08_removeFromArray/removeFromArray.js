6fb9d876493b96bfe2452e319bf7c5ba9e123016
jack <145956234+jacksld@users.noreply.github.com>
2026-03-01 09:54:22 +0700

    Start exercise 8
    Function that takes an array and a value then
    remove that value from the array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+// Remove selected arguments from the provided array
+const removeFromArray = function(array, valueToRemove) {
+    // Takes multiples arguments
+    // Find the exact matches of provided arguments in the array
+    // Remove those elements from the array
+        // Remove a single element from the array
+    let removingIndex = array.indexOf(valueToRemove);
+    array.splice(removingIndex, 1);
+    // Return the removed array
+    return array;
 };
 
 // Do not edit below this line
