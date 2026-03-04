b6fa92d1b0a348721813827d40c30f645172ce2f
jack <145956234+jacksld@users.noreply.github.com>
2026-03-04 17:22:02 +0700

    Added logic to remove all occurrences of elements
    Iterate through an array of removing element,
    check each item to see if it's exist in the
    original array or not.
    
    If it does, remove that element until none of its
    occurrences remains in the original array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    const args = Array.from(arguments);
+    // Form an array of removing elements
+    const removingElements = args.slice(1);
+    // Going through every elements in the removing array
+    for (let i = 0; i < removingElements.length; i++) {
+        // Remove until all occurrences of the value is removed from the array
+        while (array.includes(removingElements[i])) {
+            // Remove the element based on its index in the original array
+            let removingIndex = array.indexOf(removingElements[i]);
+            array.splice(removingIndex, 1);
+        }
+    }
+    // Return the removed array
+    return array;
 };
 
 // Do not edit below this line
