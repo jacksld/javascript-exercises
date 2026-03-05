99eef6a9de51f3f8c8cc42d1080514b68363e7c2
jack <145956234+jacksld@users.noreply.github.com>
2026-03-05 18:39:25 +0700

    Finish exercise 9
    Passed all tests at once

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (firstValue, secondValue) {
+    // Check if the values are not integers and if they are negative
+    if ((firstValue < 0) ||
+        (secondValue < 0) ||
+        (!Number.isInteger(firstValue)) ||
+        (!Number.isInteger(secondValue))) {
+        // Return error if they are
+        return "ERROR";
+    } else {
+        // Determine which value is smaller
+        // Create a variable to store the smaller value
+        let smallerValue = firstValue < secondValue ? firstValue : secondValue;
+        const otherValue = firstValue < secondValue ? secondValue : firstValue;
+        let sum = smallerValue;
+        // Repeat this until the smaller value is equal to the other value
+        while (smallerValue < otherValue) {
+            // Plus one to the smaller value to get the next number in the range
+            smallerValue += 1;
+            sum += smallerValue;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
