d34df078b9ea95506976e4d3142bbd1b1c0c0b72
jack <145956234+jacksld@users.noreply.github.com>
2026-03-06 13:45:12 +0700

    Finish exercise 9
    Add conditions to check if a year is divisible by
    4 and not 100 OR if a year is divisible by 400
    then it is a leap year.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    // IF year is divisible by 4 AND not divisible by 100
+    // OR year is divisible by 400 THEN
+    if (((year % 4 == 0) && (year % 100 != 0)) ||
+        (year % 400 == 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
